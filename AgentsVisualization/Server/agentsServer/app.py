78c1bd94227a93b920a869929da7f3b843e5bd70
Rebeca Davila <rebedavilaaraiza@gmail.com>
2025-11-25 13:19:18 -0600

    app lista para probar con solara

--- AgentsVisualization/Server/agentsServer/app.py
+++ AgentsVisualization/Server/agentsServer/app.py
@@ -1,62 +1,56 @@
-from randomAgents.agent import RandomAgent, ObstacleAgent
-from randomAgents.model import RandomModel
-from mesa.visualization import (
-    CommandConsole,
-    Slider,
-    SolaraViz,
-    SpaceRenderer,
-)
+from randomAgents.agent import *
+from randomAgents.model import CityModel
+
+from mesa.visualization import Slider, SolaraViz, make_space_component
 from mesa.visualization.components import AgentPortrayalStyle
 
 
-def random_portrayal(agent):
+def agent_portrayal(agent):
+
     if agent is None:
         return
 
     portrayal = AgentPortrayalStyle(
-        size=50,
-        marker="o",
-        zorder=2,
+        marker="s",
     )
 
-    if isinstance(agent, RandomAgent):
-        portrayal.update(("color", "red"))
-    elif isinstance(agent, ObstacleAgent):
-        portrayal.update(("color", "gray"))
-        portrayal.update(("marker", "s"), ("size", 125), ("zorder", 1))
+    if isinstance(agent, Road):
+        portrayal.color = "#aaa"
+
+    if isinstance(agent, Destination):
+        portrayal.color = "lightgreen"
+
+    if isinstance(agent, Traffic_Light):
+        portrayal.color = "red" if not agent.state else "green"
+
+    if isinstance(agent, Obstacle):
+        portrayal.color = "#555"
 
     return portrayal
 
 
+def post_process(ax):
+    ax.set_aspect("equal")
+
+
 model_params = {
+    "N": 5,
     "seed": {
         "type": "InputText",
         "value": 42,
         "label": "Random Seed",
     },
-    "num_agents": Slider("Number of agents", 10, 1, 50),
-    "width": Slider("Grid width", 28, 1, 50),
-    "height": Slider("Grid height", 28, 1, 50),
 }
 
+model = CityModel(model_params["N"])
 
-# Create the model using the initial parameters from the settings
-model = RandomModel(
-    num_agents=model_params["num_agents"].value,
-    width=model_params["width"].value,
-    height=model_params["height"].value,
-    seed=model_params["seed"]["value"])
-
-renderer = SpaceRenderer(
-    model,
-    backend="matplotlib",
+space_component = make_space_component(
+    agent_portrayal, draw_grid=False, post_process=post_process
 )
-renderer.draw_agents(random_portrayal)
 
 page = SolaraViz(
     model,
-    renderer,
-    components=[CommandConsole],
+    components=[space_component],
     model_params=model_params,
     name="Random Model",
 )
